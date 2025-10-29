cdc2bbf8b5165efcd42016c288b657856e84a126
flyteck <111991255+flyteck@users.noreply.github.com>
2025-10-29 15:44:42 -0400

    Update

--- gnashjs.js
+++ gnashjs.js
@@ -1,3 +1,12 @@
+//////////////////////////////Gnash JS
+
+//////////After short delay, add smooth scroll to the body to prevent links w ids from jumping on load
+setTimeout(() => {
+	document.documentElement.style.scrollBehavior = "smooth";
+}, "200");
+
+
+
 //////////Little snip that ensures the current section is highlighted
 
 //get all the sections
@@ -14,4 +23,19 @@ for (var i = sections.length - 1; i >= 0; i--) {
 		var link = nextElement.querySelector("a[href='#" + sectionID + "']");
 		link.classList.add("focused");
 	}
+}
+
+
+//////////Main image parallax
+
+//get main image, set it at the top
+var mainImage = document.getElementById("eyesimage");
+mainImage.style.top = 0;
+
+//scroll listener
+window.addEventListener("scroll", imageParallax);
+
+//using the scroll Y (divided by 5 for smaller movements) and a negative number, slide the image up/down on scroll
+function imageParallax() {
+	  mainImage.style.top = "-" + parseInt(window.scrollY)/parseInt("5") + "px";
 }
