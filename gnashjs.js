c4c1b63d863acfcb1dde9f4830a6aaa7ca1239a8
flyteck <111991255+flyteck@users.noreply.github.com>
2025-10-29 23:11:06 -0400

    Add files via upload

--- gnashjs.js
+++ gnashjs.js
@@ -51,13 +51,135 @@ function animateIn() {
 	for (var i = sectionWraps.length - 1; i >= 0; i--) {
 		var middleScreen = parseInt(window.innerHeight / "2");
 
-		console.log(middleScreen)
-
 		if(parseInt(sectionWraps[i].getBoundingClientRect().top) <= middleScreen && parseInt(sectionWraps[i].getBoundingClientRect().bottom) >= middleScreen) {
 			sectionWraps[i].style.opacity = "1"
 		} else {
 			sectionWraps[i].style.opacity = "0.1"
 		}
-
 	}
 }
+
+//////////Gallery 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Event listeners for all keypress functions
+
+document.addEventListener("keydown", keyPress);
+
+function keyPress (e) {
+  if(e.key === "Escape") {
+    escapeClose();
+  }
+
+  if(e.key === "ArrowLeft") {
+    tabLeft();
+  }
+
+  if(e.key === "ArrowRight") {
+    tabRight();
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//This lets all display images open to a modal view
+var image = document.querySelectorAll(".image-wrapper");
+//add listeners to all of the icons
+for (i = 0; i < image.length; i++) {
+  image[i].addEventListener('click', modalOpen);
+}
+
+function modalOpen() {
+  if(!this.classList.contains("open")) {
+    this.classList.add("open");
+    document.querySelector("body").classList.add("locked");
+  } else {
+    this.classList.remove("open");
+    document.querySelector("body").classList.remove("locked");
+    document.getElementById("gallery").scrollIntoView({ behavior: "instant"});
+  }
+}
+
+//This lets you close them with escape
+
+function escapeClose (e) {
+    var openModal = document.querySelectorAll(".image-wrapper.open");
+    if(openModal.length !== 0) {
+        openModal[0].classList.remove("open");
+        document.querySelector("body").classList.remove("locked");
+        document.getElementById("gallery").scrollIntoView({ behavior: "instant"});
+    }
+}
+
+//And THIS lets you close them with an x button inside
+var closeButtons = document.querySelectorAll(".close-button");
+
+for (i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener("click", buttonClose);
+}
+
+function buttonClose() {
+  var openModal = this.closest(".open");
+  openModal.classList.remove("open");
+  document.querySelector("body").classList.add("locked");
+
+}
+
+//This lets you tab left & right
+
+//This lets all display images open to a modal view
+var leftArrow = document.querySelectorAll(".left-arrow");
+//add listeners to all of the icons
+for (i = 0; i < leftArrow.length; i++) {
+  leftArrow[i].addEventListener('click', tabLeft);
+}
+
+//This lets all display images open to a modal view
+var rightArrow = document.querySelectorAll(".right-arrow");
+//add listeners to all of the icons
+for (i = 0; i < rightArrow.length; i++) {
+  rightArrow[i].addEventListener('click', tabRight);
+}
+
+function tabLeft() {
+  var openModal = document.querySelectorAll(".image-wrapper.open");
+  if(openModal.length > 0) {
+
+    if(event.key === "ArrowLeft") {
+	   openModal[0].classList.remove("open");
+	}
+    
+
+    if(openModal[0].previousElementSibling !== null && openModal[0].previousElementSibling.classList.contains("image-wrapper")) {
+      	openModal[0].previousElementSibling.classList.add("open");
+      	document.querySelector("body").classList.add("locked");
+    } else {
+    	if(document.querySelectorAll(".image-wrapper.open").length == 0) {
+    		document.querySelector("body").classList.remove("locked");
+    	}
+    	document.getElementById("gallery").scrollIntoView({ behavior: "instant"});
+    }
+  }
+}
+
+function tabRight() {
+  var openModal = document.querySelectorAll(".image-wrapper.open");
+  if(openModal.length > 0) {
+
+    if(event.key === "ArrowRight") {
+	   openModal[0].classList.remove("open");
+	}
+
+    if(openModal[0].nextElementSibling !== null && openModal[0].nextElementSibling.classList.contains("image-wrapper")) {
+      	openModal[0].nextElementSibling.classList.add("open");
+      	document.querySelector("body").classList.add("locked");
+    } else {
+      	if(document.querySelectorAll(".image-wrapper.open").length == 0) {
+    		document.querySelector("body").classList.remove("locked");
+    	}
+    	document.getElementById("gallery").scrollIntoView({ behavior: "instant"});
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
