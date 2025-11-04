4e3c3d8d6dc8245367c0e7ee0f217b7ae4c48657
flyteck <111991255+flyteck@users.noreply.github.com>
2025-11-03 19:20:03 -0500

    Add files via upload

---
+++ dialogue.js
@@ -0,0 +1,8 @@
+//Character Dialogue as a string. Splice it every (20 words?)
+
+const npcList = [
+  { name: "Alsike", dialogue: "Hey guys. I have a clover on my ass did you know? Pretty wild. Anyway, this needs to be over twenty words so I'm still writing. IDK what I'm doing man it just needs to be long enough to test, okay? Actually even longer.", repeatLine: "Anyway, off you go!"},
+  { name: "Fluffy", dialogue: "RAWR lol XD", repeatLine: "Raaaaawwwr."},
+  //Items Down Here
+  { name: "CandyApple", dialogue: "Toffee apples (in Commonwealth English) or candy apples (in American English) are whole apples covered in a sugar candy coating, with a stick inserted as a handle. These are a common treat at fall festivals in Western culture in the Northern Hemisphere, such as Halloween and Guy Fawkes Night, because these festivals occur in the wake of annual apple harvests.[1] Although toffee apples and caramel apples may seem similar, they are made using distinctly different processes."},
+];
